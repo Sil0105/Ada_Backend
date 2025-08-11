e1380d0df58924b9fec8bec80b82abfb835d4d2a
Sil0105 <pipita0105@gmail.com>
2025-08-10 22:39:33 -0300

    Agrego datos.js

---
+++ Clase02back/datos.js
@@ -0,0 +1,6 @@
+ let curso = {
+      nombreCurso: "JavaScript para principiantes",
+     duracion: "4 semanas",
+    temas: ["Variables", "Funciones", "Arrays", "Objetos"]
+};
+module.exports = curso; 
