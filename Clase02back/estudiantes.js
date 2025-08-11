e4f29fa77009a66be759d7e019d13ac662c1d2a4
Sil0105 <pipita0105@gmail.com>
2025-08-10 22:55:27 -0300

    Agrego estudiantes.js

---
+++ Clase02back/estudiantes.js
@@ -0,0 +1,8 @@
+const estudiantes = [
+    { nombre: 'Gaston', edad: 26, curso: 'Plastica', notas: [6, 9, 7] },
+    { nombre: 'Luis', edad: 18, curso: 'Física', notas: [6, 5, 10] },
+    { nombre: 'Laura', edad: 22, curso: 'Historia', notas: [9, 7, 5] },
+];
+
+
+module.exports = estudiantes;
