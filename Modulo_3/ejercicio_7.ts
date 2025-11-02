dc646f93429a57374e78e6207b97bf876b29f557
Sil0105 <pipita0105@gmail.com>
2025-11-02 16:12:30 -0300

    Agrego ejercicio_7.ts

---
+++ Modulo_3/ejercicio_7.ts
@@ -0,0 +1,8 @@
+let dia: "Lunes" | "Martes" | "Miércoles";
+
+dia = "Lunes";     
+console.log(`Día seleccionado: ${dia}`);
+
+dia = "Martes";      
+console.log(`Día seleccionado: ${dia}`);
+dia = "jueves";
