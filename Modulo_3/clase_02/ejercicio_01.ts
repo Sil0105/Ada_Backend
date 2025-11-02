188655fe5fffeb19f1e3e9be4728d3d7e13cfd08
Sil0105 <pipita0105@gmail.com>
2025-11-02 16:44:15 -0300

    Agrego ejercicio_01.ts

---
+++ Modulo_3/clase_02/ejercicio_01.ts
@@ -0,0 +1,5 @@
+function multiplicarNumeros(a: number, b: number): number {
+  return a * b;
+}
+const resultado = multiplicarNumeros(10, 3);
+console.log(`El resultado de la multiplicación es: ${resultado}`);
