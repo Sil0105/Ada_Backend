3a44d503256204d9bf7035ad0ced054e54228259
Sil0105 <pipita0105@gmail.com>
2025-11-02 20:32:11 -0300

    Agrego ejercicio_06.ts

---
+++ Modulo_3/clase_02/ejercicio_06.ts
@@ -0,0 +1,6 @@
+function nombrepersona (nombre: string, edad: number, pais: string):{
+nombre: string, edad: number, pais: string} { return {nombre,edad,pais}};
+
+
+const persona = nombrepersona("Martin", 50, "Argentina");
+console.log (persona);
