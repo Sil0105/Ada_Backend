f1c8b200c7ec66c6dfc84edab743945f568d3452
Sil0105 <pipita0105@gmail.com>
2025-12-18 21:45:35 -0300

    Modifico codigo ejercicio12.ts

--- Modulo_3/clase_12/ejercicio2.ts
+++ Modulo_3/clase_12/ejercicio2.ts
@@ -1,29 +1,33 @@
-import express, { Request, Response } from "express";
+import type { Request, Response } from "express";
+const express = require("express");
 
 const app = express();
-app.use(express.json()); 
+app.use(express.json());
 
+interface User {
+  name: string;
+  email: string;
+}
 
-const usuarios: { name: string; email: string }[] = [];
+const users: User[] = [];
 
-app.post("/usuarios", (req: Request, res: Response) => {
-    const { name, email } = req.body;
+app.post("/users", (req: Request, res: Response) => {
+  const { name, email } = req.body;
 
-    if (!name || !email) {
-        return res.status(400).json({
-            error: "Faltan datos: name y email son obligatorios",
-        });
-    }
+  if (!name || !email) {
+    return res
+      .status(400)
+      .send("Faltan datos obligatorios: name y email");
+  }
 
-    const nuevoUsuario = { name, email };
-    usuarios.push(nuevoUsuario);
+  users.push({ name, email });
 
-    res.status(201).json({
-        mensaje: "Usuario agregado correctamente",
-        usuario: nuevoUsuario,
-    });
+  res.status(201).json({
+    message: "Usuario ha sido agregado con éxito",
+    users,
+  });
 });
 
 app.listen(3000, () => {
-    console.log("🚀 Servidor funcionando en http://localhost:3000");
+  console.log("Servidor escuchando en http://localhost:3000");
 });
